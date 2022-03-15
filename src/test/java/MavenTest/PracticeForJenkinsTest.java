b2fd5b86626e12c83357b34f536c722baf1c5322
AdarshaLS <adarsha1984@gmail.com>
2022-03-15 14:15:03 +0530

    new jenkins practice

---
+++ src/test/java/MavenTest/PracticeForJenkinsTest.java
@@ -0,0 +1,13 @@
+package MavenTest;
+
+import org.testng.annotations.Test;
+
+public class PracticeForJenkinsTest {
+	
+	@Test
+	public void practiceForJenkinsTest()
+	{
+		System.out.println("Jenkins practice is going on");
+	}
+
+}
